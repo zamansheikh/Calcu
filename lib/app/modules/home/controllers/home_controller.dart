49f4aa8e1206f9fcd54cee4feb2aaddfd20ea11f
Zaman Sheikh <zaman6545@gmail.com>
2023-10-24 20:04:01 +0600

    Final Build

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:math_expressions/math_expressions.dart';
-import 'package:intl/intl.dart';
 import 'package:flutter/services.dart';
 
 class HomeController extends GetxController {
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -3,10 +3,12 @@ part of 'app_pages.dart';
 
 abstract class Routes {
   Routes._();
+  // ignore: constant_identifier_names
   static const HOME = _Paths.HOME;
 }
 
 abstract class _Paths {
   _Paths._();
+  // ignore: constant_identifier_names
   static const HOME = '/home';
 }
