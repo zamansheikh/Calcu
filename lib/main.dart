ba9112d2b02249720f1a85e2164d630e8fdcd185
Zaman Sheikh <zaman6545@gmail.com>
2024-08-14 16:38:27 +0600

    add more device

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:calcu/presentation/providers/home_provider.dart';
 import 'package:calcu/theme/app_theme.dart';
 import 'package:calcu/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 import 'injection_container.dart' as di;
 
@@ -22,21 +23,27 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => di.sl<HomeProvider>()),
-        ChangeNotifierProvider(create: (_) => di.sl<ThemeProvider>()),
-      ],
-      child: Consumer<ThemeProvider>(builder: (context, value, _) {
-        return MaterialApp(
-          title: 'Calcu App',
-          theme: AppTheme.defalultTheme,
-          home: const HomePage(),
-          themeMode: value.themeMode,
-          darkTheme: AppTheme.darkTheme,
-          debugShowCheckedModeBanner: false,
-        );
-      }),
-    );
+    return ScreenUtilInit(
+        designSize: const Size(412, 732),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (_, child) {
+          return MultiProvider(
+            providers: [
+              ChangeNotifierProvider(create: (_) => di.sl<HomeProvider>()),
+              ChangeNotifierProvider(create: (_) => di.sl<ThemeProvider>()),
+            ],
+            child: Consumer<ThemeProvider>(builder: (context, value, _) {
+              return MaterialApp(
+                title: 'Calcu App',
+                theme: AppTheme.defalultTheme,
+                home: const HomePage(),
+                themeMode: value.themeMode,
+                darkTheme: AppTheme.darkTheme,
+                debugShowCheckedModeBanner: false,
+              );
+            }),
+          );
+        });
   }
 }
